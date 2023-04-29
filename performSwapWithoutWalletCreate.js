c7c02e621e8895565d944f84189e01798c40dea2
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2023-04-29 16:17:26 +0530

    final

--- performSwapWithoutWalletCreate.js
+++ performSwapWithoutWalletCreate.js
@@ -7,52 +7,25 @@ const provider = new ethers.providers.AlchemyProvider('goerli', process.env.ALCH
 const getSigner = async () => {
     try{
     const mainWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider); //for funding the newly created wallets
-    await mainWallet.connect(provider);
-    return mainWallet;
+   return mainWallet.connect(provider);
     }
     catch(err){
         console.error('The error before getting signer is : ', err);
     }
 }
 
-const createWallets = async () => {
-        try {
-            const wallet = ethers.Wallet.createRandom();
-            const txn = {
-                to: wallet.address,
-                value: ethers.utils.parseEther('1.0') 
-            };
-            const signer = await getSigner();
-            await signer.sendTransaction(txn).then(async (tx) => {
-                await tx.wait();
-                console.log('txn', tx);
-                console.log('The balance is: ', ethers.utils.formatEther(await provider.getBalance(wallet.address)));
-            });
-           return wallet.connect(provider);
-    
-        } 
-        catch (err) {
-            console.error('The error is: ', err);
-        }
-    
-    }
-
 const uniswapRouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 const uniswapRouter = new ethers.Contract(uniswapRouterAddress, uniswapRouterAbi, provider);
 const tokenIn = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6' //address of weth token on goerli
 const tokenOut = '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984'// address of uni token on goerli
 const path = [tokenIn, tokenOut]
-const amountIn = ethers.utils.parseEther('0.2');
+const amountIn = ethers.utils.parseEther('1');
 const deadline = Math.floor(Date.now() / 1000) + 60 * 5; //5 mins
 const minAmountOut = 0;
 const gasLimit= 20000;
 const gasPrice = ethers.utils.parseUnits('500', 'gwei');
-
-const swap = async () => {
-    //for(let i=0; i<10; i++)
-    //{
-    const wallet= await createWallets().catch((err) =>
-            console.error('The error is before createWallet is: ', err))
+const trade = async () => {
+    await getSigner().then(async (wallet) => {
       const uniswapTrade = uniswapRouter.connect(wallet);
       await provider
         .getBalance(wallet.address)
@@ -96,10 +69,10 @@ const swap = async () => {
           console.log('Balance After swap: ', ethers.utils.formatEther(balance))
         )
         .catch((err) => console.error('The error after swap is: ', err));
-    //}
-    
-   
+    }).catch((err) =>
+      console.error('The error is before createWallet is: ', err)
+    );
   };
   
 
-swap().catch((err) => console.error('The error is: ', err));
+trade().catch((err) => console.error('The error is: ', err));
